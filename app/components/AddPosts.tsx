ec5249c4a3901c3ddc028d2315de6898eb7019c8
Romina <rominahelou@gmail.com>
2023-05-17 13:26:49 +0200

    Add posts

--- app/components/AddPosts.tsx
+++ app/components/AddPosts.tsx
@@ -1,19 +1,34 @@
 'use client'
 
 import { useState } from "react"
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from "axios"
 
-export default function createPost(){
+export default function createPost() {
     const [title, setTitle] = useState("")
     const [isDisabled, setIsDisabled] = useState(false)
-    return(
-        <form className="bg-white my-8 p-8 rounded-md">
+
+
+    // Create a post
+    const { mutate } = useMutation(
+        async (title: string) => await axios.post("/api/posts/addPost", { title })
+    )
+
+    const submitPost = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsDisabled(true)
+        mutate(title)
+    }
+
+    return (
+        <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
             <div className="flex flex-col my-4 ">
-                <textarea onChange={(e) => setTitle(e.target.value)} 
-                name="title" 
-                id="" 
-                value={title} 
-                placeholder="What's on your mind?"
-                className="p-4 text-lg rounded-md my-2 bg-gray-200"></textarea>
+                <textarea onChange={(e) => setTitle(e.target.value)}
+                    name="title"
+                    id=""
+                    value={title}
+                    placeholder="What's on your mind?"
+                    className="p-4 text-lg rounded-md my-2 bg-gray-200"></textarea>
             </div>
             <div className="flex items-center justify-between gap-2">
                 <p className={`font-bold text-sm ${title.length > 300 ? "text-red-700" : "text-gray-700"}`}>{`${title.length}/300`}</p>
